993ccc84d5461d06abeb650e1c66a4ab31622012
Maciek Naruszewicz <maciek.naruszewicz@gmail.com>
2012-10-25 09:31:53 +0200

    Remove shiro for JAAS testing.
    Also, clean up a bit after .gitignore ignores build directories.
    
    Signed-off-by: Maciek Naruszewicz <maciek.naruszewicz@gmail.com>

--- build/web/WEB-INF/view/logowanie.jsp
+++
@@ -1,7 +0,0 @@
-<div id="main">
-    <form action="${pageContext.request.contextPath}/autentykacja" method="post">
-        Identyfikator: <input type="text" name="identyfikator"><br/>
-        Haslo: <input type="password" name="password"><br/>
-        <input type="submit" value="Loguj sie">
-    </form>
-</div>
--- src/java/pl/gda/pg/eti/kask/projects_manager/managers/AuthenticationManager.java
+++ src/java/pl/gda/pg/eti/kask/projects_manager/managers/AuthenticationManager.java
@@ -1,13 +1,9 @@
 package pl.gda.pg.eti.kask.projects_manager.managers;
 
-import org.apache.shiro.realm.ldap.JndiLdapRealm;
-
 public class AuthenticationManager {
     
-    private JndiLdapRealm ldapRealm;
-    
     public AuthenticationManager() {
-       // ldapRealm = new JndiLdapRealm();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                b
+        
     }
     
     public boolean userExists(String ident, String password) {
