afc1159440135b6272ab2ba2fce6ba453a08cba4
mateusz <mateusz@crunchbang>
2012-10-30 19:05:00 +0100

    Added method for editing projects

--- src/java/pl/gda/pg/eti/kask/projects_manager/form/EditHelperBean.java
+++ src/java/pl/gda/pg/eti/kask/projects_manager/form/EditHelperBean.java
@@ -7,6 +7,7 @@ package pl.gda.pg.eti.kask.projects_manager.form;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 import pl.gda.pg.eti.kask.projects_manager.entity.Projects;
 import pl.gda.pg.eti.kask.projects_manager.facade.ProjectsFacade;
 
@@ -14,25 +15,54 @@ import pl.gda.pg.eti.kask.projects_manager.facade.ProjectsFacade;
  *
  * @author mateusz
  */
-@ManagedBean(name = "EditHeperBean")
+@ManagedBean(name = "EditHelperBean")
 @SessionScoped
 public class EditHelperBean {
-    
-    
+
     @EJB
     private ProjectsFacade projectFacadeLocal;
     private Projects localProjects;
     private boolean editingProjects;
-    
+
+    public EditHelperBean() {
+    }
+
     public Projects getProjects() {
         return localProjects;
     }
-    
+
     public String newProject() {
         localProjects = new Projects();
         editingProjects = false;
-        
-        return "editProject";
+
+        return "edit_projects";
     }
-    
+
+    public String editProject() {
+        localProjects = (Projects) FacesContext.getCurrentInstance()
+                .getExternalContext().getRequestMap()
+                .get("Projects");
+        editingProjects = true;
+
+        return "edit_projects";
+
+    }
+
+    public String saveProject() {
+        if (editingProjects) {
+            projectFacadeLocal.edit(localProjects);
+        } else {
+            projectFacadeLocal.create(localProjects);
+        }
+
+        return "projects_list";
+    }
+
+    public String usunProject() {
+        localProjects = (Projects) FacesContext.getCurrentInstance()
+                .getExternalContext().getRequestMap()
+                .get("Projects");
+        projectFacadeLocal.remove(localProjects);
+        return "projects_list";
+    }
 }
