a8a6d1442cbec2b8088dcde29c6d39bb9073a1ca
Maciej Naruszewicz <maciej.naruszewicz@intel.com>
2012-11-02 11:02:16 +0100

    Add abstract repositories manager.
    This class will hold templates for methods accessing the repositories,
    adding or removing any users or repositories themselves.
    
    Signed-off-by: Maciej Naruszewicz <maciej.naruszewicz@intel.com>

---
+++ src/java/pl/gda/pg/eti/kask/projects_manager/managers/AbstractRepositoriesManager.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.gda.pg.eti.kask.projects_manager.managers;
+
+/**
+ *
+ * @author maryl
+ */
+public class AbstractRepositoriesManager {
+    
+}
