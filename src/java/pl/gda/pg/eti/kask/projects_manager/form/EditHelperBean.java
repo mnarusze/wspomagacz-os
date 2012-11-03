b69b3811e526046869ed8b1902d5e96383e10954
Mateusz <Mateusz@Mateusz-PC>
2012-11-03 19:07:34 +0100

    Changes to editing and adding users.

--- src/java/pl/gda/pg/eti/kask/projects_manager/form/EditHelperBean.java
+++ src/java/pl/gda/pg/eti/kask/projects_manager/form/EditHelperBean.java
@@ -9,7 +9,9 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import pl.gda.pg.eti.kask.projects_manager.entity.Projects;
+import pl.gda.pg.eti.kask.projects_manager.entity.Users;
 import pl.gda.pg.eti.kask.projects_manager.facade.ProjectsFacade;
+import pl.gda.pg.eti.kask.projects_manager.facade.UsersFacade;
 import pl.gda.pg.eti.kask.projects_manager.managers.RepositoriesManager;
 
 /**
@@ -24,12 +26,20 @@ public class EditHelperBean {
     private ProjectsFacade projectFacadeLocal;
     private Projects localProjects;
     private boolean editingProjects;
+    @EJB
+    private UsersFacade usersFacadeLocal;
+    private Users localUsers;
+    private boolean editingUsers;
 
     public EditHelperBean() {
     }
 
     public Projects getProjects() {
         return localProjects;
+    }
+
+    public Users getUser() {
+        return localUsers;
     }
 
     public String newProject() {
@@ -49,6 +59,73 @@ public class EditHelperBean {
 
     }
 
+    public String addUsersToProject() {
+        localProjects = (Projects) FacesContext.getCurrentInstance()
+                .getExternalContext().getRequestMap()
+                .get("Projects");
+
+
+        return "users_list";
+
+    }
+
+    public String saveUsersToProject() {
+        localUsers = (Users) FacesContext.getCurrentInstance()
+                .getExternalContext().getRequestMap()
+                .get("Users");
+
+        localProjects.getUsersCollection().add(localUsers);
+
+        projectFacadeLocal.edit(localProjects);
+        return "projects_list";
+    }
+
+    public String removeUsersFromProject() {
+        localUsers = (Users) FacesContext.getCurrentInstance()
+                .getExternalContext().getRequestMap()
+                .get("Users");
+
+        localProjects.getUsersCollection().remove(localUsers);
+
+        projectFacadeLocal.edit(localProjects);
+        return "projects_list";
+    }
+
+    public String deleteUsers() {
+        localUsers = (Users) FacesContext.getCurrentInstance()
+                .getExternalContext().getRequestMap()
+                .get("User");
+
+        usersFacadeLocal.remove(localUsers);
+        projectFacadeLocal.edit(localProjects);
+        return "projects_list";
+    }
+
+    public String editUsers() {
+        localUsers = (Users) FacesContext.getCurrentInstance()
+                .getExternalContext().getRequestMap()
+                .get("User");
+        editingUsers = true;
+
+        return "edit_user";
+    }
+
+    public String newUser() {
+        localUsers = new Users();
+        editingUsers = false;
+
+        return "edit_user";
+    }
+    
+    public String saveUser() {
+        if (editingUsers) {
+            usersFacadeLocal.edit(localUsers);
+        } else {
+            usersFacadeLocal.create(localUsers);
+        }
+        return "projects_list";
+    }
+
     public String saveProject() {
         if (editingProjects) {
             projectFacadeLocal.edit(localProjects);
@@ -60,7 +137,7 @@ public class EditHelperBean {
                 RepositoriesManager.createRepository(localProjects.getProjName(), "svn");
             }
             projectFacadeLocal.create(localProjects);
-            
+
         }
 
         return "projects_list";
